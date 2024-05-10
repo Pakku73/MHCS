358d394c0d428b1147a7b6889cabe31b3997c13b
Pakku73 <12monstroplantes@gmail.com>
2024-05-10 16:49:09 +0200

    commit

---
+++ src/containers/home/accompagnement.tsx
@@ -0,0 +1,36 @@
+export const accompagnement = () => {
+    return <>
+    
+      <section className="flex flex-col bg-white">
+              <div className="flex flex-col bg-[#e8e8e8]">
+                <h1>
+                    Un accompagnement dédié, pour des travaux en toute
+                    <span>serenite</span>
+                </h1>
+                <div className="flex">
+                    <img src="../../../public/assets/images/support-visual.jpg" alt="" />
+
+                    <div>
+                        <h2>Pourquoi choisir MHCS ?</h2>
+                        <p>
+                            MHCS trouve son essence dans la volonté de répondre 
+                            de manière précise et efficace aux besoins et aux exigences 
+                            spécifiques du monde hôtelier. Notre expertise s'est solidement 
+                            établie pour vous offrir des conseils avisés et éclairés, 
+                            guidant chaque étape de la maintenance et de la rénovation 
+                            au sein de votre établissement.
+                        </p>
+                        <p>
+                            Nous sommes fiers de notre capacité à conjuguer performance 
+                            et durabilité, offrant ainsi à nos clients une approche globale, 
+                            pragmatique et responsable pour faire face aux défis actuels 
+                            du secteur hôtelier.
+                        </p>
+                    </div>
+                </div>
+
+              </div>
+      </section>
+  
+    </>;
+  };
