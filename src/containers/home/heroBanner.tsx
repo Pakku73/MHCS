a8b3d7df40cf33090c6468eccab81eaf5a84c39f
Pakku73 <12monstroplantes@gmail.com>
2024-05-10 16:13:17 +0200

    commit

--- src/containers/home/heroBanner.tsx
+++ src/containers/home/heroBanner.tsx
@@ -3,18 +3,22 @@
 export const HeroBanner = () => {
   return <>
   
-    <section className="bg-[url('../../../public/images/hero-banner.jpg')] bg-no-repeat">
+    <section className="flex flex-col bg-[url('../../../public/images/hero-banner.jpg')] bg-no-repeat">
             <div className="flex flex-col">
-            <h1>
-              Votre service de
-            </h1>
-            <h1>
-              maintenance hôtelière
-            </h1>
-            <h1>
-              Sur-mesure, 24H/24.
-            </h1>
+              <h1>
+                Votre service de
+              </h1>
+              <h1>
+                maintenance hôtelière
+              </h1>
+              <h1>
+                Sur-mesure, 24H/24.
+              </h1>
+            </div>
 
+            <div className="flex">
+              <button className="bg-white color-[#000000]">Prendre rendez-vous</button>
+              <button>Découvrir les prestations</button>
             </div>
     </section>
 
