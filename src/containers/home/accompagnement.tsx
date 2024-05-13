b4e9153d0b8dca757f5bf9f516eeba4fc8bfb608
Draxx0 <williamfort.lmgl@gmail.com>
2024-05-13 23:04:19 +0200

    :pencil2: 13-05-24 Work's feedback

--- src/containers/home/accompagnement.tsx
+++ src/containers/home/accompagnement.tsx
@@ -1,43 +1,51 @@
 export const Accompagnement = () => {
-    return <>
-    
+  return (
+    <>
       <section>
+        {/* Ici aussi attention, pas de vh pour positionner tes sections ( d'ailleurs attention tu utilise le padding pour créer du blanc sur x & y, normalement, 
+            c'est ta section qui devrait avoir ce rôle, de plus renseigne toi sur les max-width, qui vont te permettre d'ajuster la taille maximal de tes blocks pour créer 
+            les espaces blanc en x et avec du margin en y tu vas pouvoir créer le blanc en y (PS : les max-width sont un avantage pour le responsive, on en reparleras je te montrerais)  ) */}
         <div className="flex flex-col bg-[#fffcf8] p-[10vh] pr-[20vh] pl-[20vh]">
+          <div className="flex flex-col bg-[#ffffff] shadow-md p-10 rounded-[6px]">
+            <h2 className="flex flex-col font-ChangeOne text-[500] text-[30px] m-auto text-center w-[600px]">
+              Un accompagnement dédié, pour des travaux en toute
+              <span className="font-Neonderthaw text-[50px]">serenite</span>
+            </h2>
+            <div className="flex">
+              <img
+                className="rounded-[6px]"
+                src="../../../public/assets/images/support-visual.jpg"
+                alt=""
+              />
 
-            <div className="flex flex-col bg-[#ffffff] shadow-md p-10 rounded-[6px]">
-                <h2 className="flex flex-col font-ChangeOne text-[500] text-[30px] m-auto text-center w-[600px]">
-                    Un accompagnement dédié, pour des travaux en toute
-                    <span className="font-Neonderthaw text-[50px]">serenite</span>
+              {/* Oupsi attention, je vois que tu as utilisé le padding pour centrer l'élément, quand tu dois centrer quelque choses et dans 80% des cas quand tu dois gérer le comportement de placement de contenu, tu utiliseras les flexboxs
+              Si tu n'est pas encore à l'aise avec je te conseilles https://flexboxfroggy.com/#fr qui est une référence dans l'apprentissage de ces dernières. */}
+              <div className="text-[#000000] p-20">
+                {/* Pour harmoniser les tailles de polices, on essaie de se référencer au default tailwind (xs, sm, md, lg, xl etc..) que l'on viens modifier dans le tailwind.config.js pour l'ajuster au font sizes de la maquettes ( par le biais de l'objet extend ) */}
+                <h2 className="font-ChangeOne text-[20px]">
+                  Pourquoi choisir MHCS ?
                 </h2>
-                <div className="flex">
-                    <img className="rounded-[6px]" src="../../../public/assets/images/support-visual.jpg" alt="" />
-
-                    <div className="text-[#000000] p-20">
-                        <h2 className="font-ChangeOne text-[20px]">Pourquoi choisir MHCS ?</h2>
-                        <br/>
-                        <p>
-                            MHCS trouve son essence dans la volonté de répondre 
-                            de manière précise et efficace aux besoins et aux exigences 
-                            spécifiques du monde hôtelier. Notre expertise s'est solidement 
-                            établie pour vous offrir des conseils avisés et éclairés, 
-                            guidant chaque étape de la maintenance et de la rénovation 
-                            au sein de votre établissement.
-                        </p>
-                        <br/>
-                        <p>
-                            Nous sommes fiers de notre capacité à conjuguer performance 
-                            et durabilité, offrant ainsi à nos clients une approche globale, 
-                            pragmatique et responsable pour faire face aux défis actuels 
-                            du secteur hôtelier.
-                        </p>
-                    </div>
-                </div>
-
+                <br />
+                <p>
+                  MHCS trouve son essence dans la volonté de répondre de manière
+                  précise et efficace aux besoins et aux exigences spécifiques
+                  du monde hôtelier. Notre expertise s'est solidement établie
+                  pour vous offrir des conseils avisés et éclairés, guidant
+                  chaque étape de la maintenance et de la rénovation au sein de
+                  votre établissement.
+                </p>
+                <br />
+                <p>
+                  Nous sommes fiers de notre capacité à conjuguer performance et
+                  durabilité, offrant ainsi à nos clients une approche globale,
+                  pragmatique et responsable pour faire face aux défis actuels
+                  du secteur hôtelier.
+                </p>
+              </div>
             </div>
-
+          </div>
         </div>
-
       </section>
-  
-    </>;
-  };
+    </>
+  );
+};
--- src/containers/home/heroBanner.tsx
+++ src/containers/home/heroBanner.tsx
@@ -1,43 +1,56 @@
-// import { Button } from '../../components/common/Button';
-
 export const HeroBanner = () => {
-  return <>
-  
-    <section className="flex flex-col bg-no-repeat">
+  return (
+    // Pas besoin d'ajouter ce ' <> ', c'est ce que l'on appelle une fragement, en React ton jsx dois constamment être wrappé dans un seul parent
+    // et quand ce n'est pas possible, car tu voudrais que les deux éléments que tu as soit côte à côte dans le DOM, on ajoute un fragment à la place
+    // d'un div, le fragment ne rend rien dans le dom, ce qui te permet de ne pas avoir une div qui wrappe tes deux éléments, mais ces
+    // derniers directement rendu côte a côte sans parents (je te ferais une demo à mon retour fais moi y penser !)
+    <>
+      {/* Ici essaie plutôt d'utiliser le composant Section en wrapper de ton contenu, cela te permettra de le réutiliser constamment afin que les classes tailwind ou les potentiels props
+      que tu souhaiterais lui passer soit centraliser au même endroit ( en l'occurence, sur le composant Section ). Je te conseil de te renseigner sur ce qu'est
+      'children' en react, on en as un peu parler, tu en auras besoin pour crée le composant Section. */}
+      <section className="flex flex-col bg-no-repeat">
+        <div>
+          <div className="flex flex-col mt-80 absolute z-15 h-full w-full">
+            {/* Attention à l'utilisation des margin, en général quand ton margin sur l'axe x dépasse '4' c'est que tu t'y prend mal ( sauf certain cas d'exception ) ici, il vaut mieux essayer de centrer le bloc. */}
+            <h1 className="flex flex-col mb-10 justify-center items-center ml-50 z-10 text-[500] gap-4 text-6xl text-center">
+              <span className="font-ChangeOne uppercase text-[#f2ead5] [text-shadow:_0_4px_0_rgb(0_0_0_/_14%)]">
+                Votre service de
+              </span>
+              <span className="font-Neonderthaw text-[68px] text-[#f2ead5] [text-shadow:_0_4px_0_rgb(0_0_0_/_14%)]">
+                maintenance hôtelière
+              </span>
+              <span className="font-ChangeOne uppercase text-[#f2ead5] [text-shadow:_0_4px_0_rgb(0_0_0_/_14%)]">
+                Sur-mesure, 24H/24.
+              </span>
+            </h1>
 
-    <div>
+            <div className="flex z-10 justify-center items-center gap-10">
+              <button className="bg-[#f2ead5] text-black p-2 rounded-[2px] hover:bg-[#000000] hover:text-[#f2ead5]">
+                Prendre rendez-vous
+              </button>
+              <button className="bg-[#ada796] text-white p-2 rounded-[2px] hover:bg-[#ffffff] hover:text-[#ada796]">
+                Découvrir les prestations
+              </button>
+            </div>
+          </div>
 
-      <div className="flex flex-col mt-80 absolute z-15 h-full w-full">
-
-        <h1 className="flex flex-col mb-10 justify-center items-center ml-50 z-10 text-[500] gap-4 text-6xl text-center">
-            <span className="font-ChangeOne uppercase text-[#f2ead5] [text-shadow:_0_4px_0_rgb(0_0_0_/_14%)]">Votre service de</span>
-            <span className="font-Neonderthaw text-[68px] text-[#f2ead5] [text-shadow:_0_4px_0_rgb(0_0_0_/_14%)]">maintenance hôtelière</span>
-            <span className="font-ChangeOne uppercase text-[#f2ead5] [text-shadow:_0_4px_0_rgb(0_0_0_/_14%)]">Sur-mesure, 24H/24.</span>
-        </h1>
-
-        <div className="flex z-10 justify-center items-center gap-10">
-          <button className="bg-[#f2ead5] text-black p-2 rounded-[2px] hover:bg-[#000000] hover:text-[#f2ead5]">Prendre rendez-vous</button>
-          <button className="bg-[#ada796] text-white p-2 rounded-[2px] hover:bg-[#ffffff] hover:text-[#ada796]">Découvrir les prestations</button>
+          <div>
+            {/* L'index par défaut est "0" pas besoin du z-0 */}
+            <div className="absolute z-0 top-0 left-0 w-full bg-gradient-to-b from-[#ffffff99] to-[#00000073] h-full"></div>{' '}
+            {/*div 1*/}
+            {/* Utilisation maladroite des vh, étant donné qu'ils dépendent de la hauteur de l'écran de l'utilisateur, on ne les utilises que pour définir la hauteur d'un bloc / contenu */}
+            <img
+              className="-mt-[5.5vh] h-full"
+              src="../../../public/assets/images/hero-banner.jpg"
+              alt=""
+            />{' '}
+            {/*div 2*/}
+          </div>
         </div>
-
-      </div>
-
-      <div>
-        <div className="absolute z-0 top-0 left-0 w-full bg-gradient-to-b from-[#ffffff99] to-[#00000073] h-full"></div> {/*div 1*/}
-        <img className="-mt-[5.5vh] h-full" src="../../../public/assets/images/hero-banner.jpg" alt="" /> {/*div 2*/}
-      </div>
-      
-
-
-    </div>
-
-    </section>
-
-  </>;
+      </section>
+    </>
+  );
 };
 
-
-
-
-// bg-[url('../../../public/assets/images/hero-banner.jpg')] 
+// bg-[url('../../../public/assets/images/hero-banner.jpg')]
 // #50d71e
