8a05c1e5a93fc0e3046de8fa8a1d6fd8bc750871
Pakku73 <12monstroplantes@gmail.com>
2024-05-14 16:43:28 +0200

    Section chiffres

--- src/containers/home/chiffres.tsx
+++ src/containers/home/chiffres.tsx
@@ -3,16 +3,20 @@ export const Chiffres = () => {
     
     <section className="flex flex-col items-center max-w-screen-2xl m-auto mt-[100px] gap-6">
 
-        <h2 className="flex flex-col font-Arial text-center text-[20px] ">
+        <h2 className="flex flex-col font-Arial text-center text-[20px]">
             Quelques chiffres clés
         </h2>
 
-        <div className="flex justify-around">
+        <div className="flex justify-around items-center">
             <div className="border-[2px] border-[#ada796] p-1 pl-10 rounded-[4px]">
                 <h2 className="uppercase font-ChangeOne text-[20px]">D'expertises</h2>
             </div>
 
-            <div className="border-[2px] border-[#ada796] p-1 pl-10 rounded-[4px]">
+            <div className="border-[2px] border-[#ada796] p-1 pl-10 rounded-[4px] mr-[5vw] ml-[10vw]">
+                <h2 className="uppercase font-ChangeOne text-[20px]">D'expertises</h2>
+            </div>
+
+            <div className="border-[2px] border-[#ada796] p-1 pl-10 rounded-[4px] mr-[10vw] ml-[5vw]">
                 <h2 className="uppercase font-ChangeOne text-[20px]">D'expertises</h2>
             </div>
 
