79f1149217a39807e50e2594c1b2ddad8775ea32
Pakku73 <12monstroplantes@gmail.com>
2024-05-17 10:21:55 +0200

    Finalisation page contact

--- src/containers/contact/form.tsx
+++ src/containers/contact/form.tsx
@@ -5,9 +5,12 @@ export const Form = () => {
     
       <section className="flex flex-col items-center justify-center mb-[100px]">
   
-          <div className="flex flex-col items-center justify-center absolute bg-[#ada796] rounded-[10px] py-10 z-10">
+          <div className="flex flex-col items-center justify-center absolute bg-[#ada796] max-w-[900px] rounded-[10px] py-2 z-10">
     
-            <h1 className="uppercase font-ChangeOne text-white text-[40px]">Contactez-nous</h1>
+            <div>
+              <img className="translate-x-[290px] translate-y-2 w" src="../../../public/assets/icons/mail-white.svg" alt="" />
+              <h1 className="uppercase font-ChangeOne text-white text-[40px]">Contactez-nous</h1>
+            </div>
             <p className="text-[#f2ead5] text-[18px]">Nos équipes vous contactent sous 48 heures !</p>
       
             <div className="flex flex-col items-center justify-center mt-6 gap-6 max-w-full">
@@ -33,12 +36,13 @@ export const Form = () => {
     
           </div>
 
-        <div className="flex flex-col items-center justify-center absolute bg-white">
+        <div className="flex items-center justify-center absolute translate-y-[290px] bg-white z-20 max-w-[900px] rounded-[10px] py-5 px-6 text-[18px] gap-4">
+          <img src="../../../public/assets/icons/contact-popup.svg" alt="" />
           <p>Ou contactez-nous directement par téléphone <span>07.72.50.38.81</span> ou par mail <span>secretariat@mhcsparis.com.</span></p>
         </div>
   
         <div>
-          <div className="absolute z-0 top-0 left-0 w-full h-full bg-[#00000073]"></div> 
+          <div className="absolute top-0 left-0 w-full h-full bg-[#00000073]"></div> 
           {/* bg-gradient-to-b from-[#ffffff99] to-[#00000073]   */}
           <img className="-mt-[55px] h-[1000px]" src="../../../public/assets/images/contact-hero-banner.jpg" alt="" />
         </div>
