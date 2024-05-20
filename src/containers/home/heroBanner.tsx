f9697ee4829bd4e0d6a7d379b73f2ccb63a966fe
William29302 <william@marketingbox.fr>
2024-05-20 14:16:14 +0200

    :bug: fix hero banner

--- src/containers/home/heroBanner.tsx
+++ src/containers/home/heroBanner.tsx
@@ -1,45 +1,48 @@
 // import { Button } from '../../components/common/Button';
 
 export const HeroBanner = () => {
-  return <>
-  
-    <section className="flex flex-col">
+  return (
+    <section className="h-svh mt-[90px]">
+      <div className="relative h-full flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center z-15 h-full w-full">
+          <h1 className="flex flex-col mb-10 justify-center items-center ml-50 z-10 gap-4 text-6xl text-center">
+            <span className="font-ChangeOne uppercase text-[#f2ead5] [text-shadow:_0_4px_0_rgb(0_0_0_/_14%)]">
+              Votre service de
+            </span>
+            <span className="font-Neonderthaw text-[68px] text-[#f2ead5] [text-shadow:_0_4px_0_rgb(0_0_0_/_14%)]">
+              maintenance hôtelière
+            </span>
+            <span className="font-ChangeOne uppercase text-[#f2ead5] [text-shadow:_0_4px_0_rgb(0_0_0_/_14%)]">
+              Sur-mesure, 24H/24.
+            </span>
+          </h1>
 
-    <div>
-
-      <div className="flex flex-col mt-80 absolute z-15 h-full w-full">
-
-        <h1 className="flex flex-col mb-10 justify-center items-center ml-50 z-10 text-[500] gap-4 text-6xl text-center">
-            <span className="font-ChangeOne uppercase text-[#f2ead5] [text-shadow:_0_4px_0_rgb(0_0_0_/_14%)]">Votre service de</span>
-            <span className="font-Neonderthaw text-[68px] text-[#f2ead5] [text-shadow:_0_4px_0_rgb(0_0_0_/_14%)]">maintenance hôtelière</span>
-            <span className="font-ChangeOne uppercase text-[#f2ead5] [text-shadow:_0_4px_0_rgb(0_0_0_/_14%)]">Sur-mesure, 24H/24.</span>
-        </h1>
-
-        <div className="flex z-10 justify-center items-center gap-10 font-ChangeOne">
-          <button className="bg-[#f2ead5] text-black p-2 rounded-[2px] hover:bg-[#000000] hover:text-[#f2ead5] transition ease-in-out duration-300  hover:-translate-y-2 shadow-md">Prendre rendez-vous</button>
-          <button className="bg-[#ada796] text-white p-2 rounded-[2px] hover:bg-[#ffffff] hover:text-[#ada796] transition ease-in-out duration-300  hover:-translate-y-2 shadow-md">Découvrir les prestations</button>
-          {/* <Button text="Prendre rendez-vous" variant="cream" />
+          <div className="flex z-10 justify-center items-center gap-10 font-ChangeOne">
+            <button className="bg-[#f2ead5] text-black p-2 rounded-[2px] hover:bg-[#000000] hover:text-[#f2ead5] transition ease-in-out duration-300  hover:-translate-y-2 shadow-md">
+              Prendre rendez-vous
+            </button>
+            <button className="bg-[#ada796] text-white p-2 rounded-[2px] hover:bg-[#ffffff] hover:text-[#ada796] transition ease-in-out duration-300  hover:-translate-y-2 shadow-md">
+              Découvrir les prestations
+            </button>
+            {/* <Button text="Prendre rendez-vous" variant="cream" />
           <Button text="Découvrir" variant="olive" /> */}
+          </div>
         </div>
 
+        <div className="absolute h-full w-full">
+          <div className="absolute z-0 top-0 left-0 w-full bg-gradient-to-b from-[#ffffff99] to-[#00000073] h-full"></div>{' '}
+          {/*div 1*/}
+          <img
+            className="h-full w-full object-cover"
+            src="../../../public/assets/images/hero-banner.jpg"
+            alt=""
+          />{' '}
+          {/*div 2*/}
+        </div>
       </div>
-
-      <div>
-        <div className="absolute z-0 top-0 left-0 w-full bg-gradient-to-b from-[#ffffff99] to-[#00000073] h-full"></div> {/*div 1*/}
-        <img className="-mt-[5.5vh] h-full" src="../../../public/assets/images/hero-banner.jpg" alt="" /> {/*div 2*/}
-      </div>
-      
-
-
-    </div>
-
     </section>
-
-  </>;
+  );
 };
 
-
-
-
-// bg-[url('../../../public/assets/images/hero-banner.jpg')] 
+// bg-[url('../../../public/assets/images/hero-banner.jpg')]
 // #50d71e
